5672fca0b85cd231a8d9b524f2b1cd503d7c1123
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-11-20 01:13:47 -0800

    start up

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      createdBy: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+      },
+      size: {
+        type: String,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+        default: 'Large'
+      },
+      toppings: [],
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
+  )
+
+//get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+});
+
+// Create the pizza model using the PizzaSchema
+const Pizza = model('Pizza',PizzaSchema);
+
+// export the pizza model
+module.exports = Pizza;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mangoos = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mangoos.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Use this to log mongo queries begin executed!
+mangoos.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
